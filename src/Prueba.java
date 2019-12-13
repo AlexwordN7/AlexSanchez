de2ea3eb3ec537cea0d5612dd304d018dc4ea17b
alexs <alexs@LAPTOP-KQGHEVBO>
2019-12-13 20:12:12 +0100

    Cambio elevarNumero

--- src/Prueba.java
+++ src/Prueba.java
@@ -21,14 +21,22 @@ public class Prueba {
 		total=operador1+operador2;
 		return total;
 	}
+	public static int elevarNumero(int operador1, int operador2) {
+		int total =0;
+		total=operador1+operador2;
+		return total;
+	}
 
 	public static void main(String[] args) {
 		Scanner ejercicio1 =new Scanner(System.in);
 		int numeros = 0;
+		System.out.println("Introduce un numero");
+		numeros=ejercicio1.nextInt();
 		suma(numeros, numeros);
 		resta(numeros,numeros);
 		multiplicacion(numeros,numeros);
 		division(numeros,numeros);
+		elevarNumero(numeros,numeros);
 		
 	}
 
