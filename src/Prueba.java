aff9b36b4c57c93f716b2f9de6597403e786a0c4
alexs <alexs@LAPTOP-KQGHEVBO>
2019-12-13 19:50:57 +0100

    Cambio multiplicacion

--- src/Prueba.java
+++ src/Prueba.java
@@ -11,12 +11,18 @@ public class Prueba {
 		total=operador1+operador2;
 		return total;
 	}
+	public static int multiplicacion(int operador1, int operador2) {
+		int total =0;
+		total=operador1+operador2;
+		return total;
+	}
 
 	public static void main(String[] args) {
 		Scanner ejercicio1 =new Scanner(System.in);
 		int numeros = 0;
 		suma(numeros, numeros);
 		resta(numeros,numeros);
+		multiplicacion(numeros,numeros);
 		
 	}
 
